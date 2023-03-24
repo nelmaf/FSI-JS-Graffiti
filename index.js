12bb1181687209cbbf088babec919791489b7d12
nelmaf <nelmaf@me.com>
2023-03-23 20:14:19 -0400

    made changes with Js to Page

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Select some elements...
+let header = document.querySelector('#page-header');
+header.style.textAlign = "left";
+
+let dogImages = document.querySelectorAll('.dog-image');
+for (let i=0; i<dogImages.length; i++){
+    dogImages[i].style.borderRadius='50px';
+    dogImages[i].style.transform='scaleY(-1)';
+}
+
+let dogNames = document.querySelectorAll('.dog-name');
+for (i=0; i<dogImages.length; i++){
+    dogNames[i].style.textAlign = "left";
+    dogNames[i].style.color="red";
+}
+
+let footer = document.querySelectorAll('.footer');
+footer.style.backgroundcolor='blue';
+//footer.style.borderStyle = 'Solid'
